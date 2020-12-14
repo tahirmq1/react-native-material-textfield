cac04ba42ac045d6f892f13ec333148b7da1ead0
Mohammed Taher <thr@Mohammeds-MacBook-Pro.local>
2020-12-14 19:46:41 +0300

    fixed proptypes

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Animated } from 'react-native';
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { Animated } from "react-native";
 
-import styles from './styles';
+import styles from "./styles";
 
 export default class Helper extends PureComponent {
   static propTypes = {
@@ -11,7 +11,7 @@ export default class Helper extends PureComponent {
 
     disabled: PropTypes.bool,
 
-    style: Animated.Text.propTypes.style,
+    style: PropTypes.any,
 
     baseColor: PropTypes.string,
     errorColor: PropTypes.string,
@@ -27,7 +27,7 @@ export default class Helper extends PureComponent {
     let opacity = focusAnimation.interpolate({
       inputRange: [-1, -0.5, 0],
       outputRange: [1, 0, 1],
-      extrapolate: 'clamp',
+      extrapolate: "clamp",
     });
 
     this.state = {
@@ -39,8 +39,7 @@ export default class Helper extends PureComponent {
   componentDidMount() {
     let { focusAnimation } = this.props;
 
-    this.listener = focusAnimation
-      .addListener(this.onAnimation.bind(this));
+    this.listener = focusAnimation.addListener(this.onAnimation.bind(this));
   }
 
   componentWillUnmount() {
@@ -63,18 +62,9 @@ export default class Helper extends PureComponent {
 
   render() {
     let { errored, opacity } = this.state;
-    let {
-      style,
-      title,
-      error,
-      disabled,
-      baseColor,
-      errorColor,
-    } = this.props;
+    let { style, title, error, disabled, baseColor, errorColor } = this.props;
 
-    let text = errored?
-      error:
-      title;
+    let text = errored ? error : title;
 
     if (null == text) {
       return null;
@@ -83,9 +73,7 @@ export default class Helper extends PureComponent {
     let textStyle = {
       opacity,
 
-      color: !disabled && errored?
-        errorColor:
-        baseColor,
+      color: !disabled && errored ? errorColor : baseColor,
     };
 
     return (
